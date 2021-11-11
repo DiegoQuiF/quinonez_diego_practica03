f4a3f1a6400260e3126b2d5c508a952de251e2b5
Diego <Diego@192.168.1.19>
2021-11-10 20:15:45 -0500

    10mo commit

---
+++ src/main/java/Modelo/Estacion.java
@@ -0,0 +1,15 @@
+package Modelo;
+
+/**
+ * @author Diego Quiñonez Flores
+ */
+
+public class Estacion {
+    private int codigo;
+    private String nombre;
+    private String direccion;
+    
+    
+    
+    
+}
---
+++ src/main/java/Modelo/Movimiento.java
@@ -0,0 +1,20 @@
+package Modelo;
+
+/**
+ * @author Diego quiñonez Flores
+ */
+
+public class Movimiento {
+    private int codigo;
+    private String tipo;
+    private int dd;
+    private int mm;
+    private int aaaa;
+    private float monto;
+    
+    
+    
+    
+    
+    
+}
