437763202e2740167352c0674c1b22d4d84d9b84
Diego <Diego@192.168.1.19>
2021-11-10 20:01:10 -0500

    8vo commit

---
+++ src/main/java/Modelo/TipoDocumento.java
@@ -0,0 +1,17 @@
+package Modelo;
+
+public class TipoDocumento {
+    private String nombre;
+    private int codigo;
+
+    public TipoDocumento(String nombre, int codigo) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+    }
+    
+    
+    
+    
+    
+    
+}
--- src/main/java/Modelo/User.java
+++ src/main/java/Modelo/User.java
@@ -11,27 +11,27 @@ public class User {
     private String paterno;
     private String materno;
     private String nombres;
+    private TipoDocumento tipoDoc;
     private String documento;
-    private String tipoDocumento;
     private Tarjeta tarjetas[];
     private int indiceTarjeta;
     private Tarjeta porDefecto;
 
-    public User(String paterno, String materno, String nombres, String documento, String tipoDocumento) {
+    public User(String paterno, String materno, String nombres, String nomDoc, int codDoc, String documento) {
         this.paterno = paterno;
         this.materno = materno;
         this.nombres = nombres;
+        this.tipoDoc = new TipoDocumento(nomDoc, codDoc);
         this.documento = documento;
-        this.tipoDocumento = tipoDocumento;
         this.indiceTarjeta = 0;
         this.tarjetas = new Tarjeta[Configuracion.maxTarjetasUser];
     }
     
-    public User(String paterno, String nombres, String documento, String tipoDocumento) {
+    public User(String paterno, String nombres, String nomDoc, int codDoc, String documento) {
         this.paterno = paterno;
         this.nombres = nombres;
+        this.tipoDoc = new TipoDocumento(nomDoc, codDoc);
         this.documento = documento;
-        this.tipoDocumento = tipoDocumento;
         this.indiceTarjeta = 0;
         this.tarjetas = new Tarjeta[Configuracion.maxTarjetasUser];
     }
